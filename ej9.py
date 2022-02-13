adfc40b22be778ef02bcb1d9a72d67879efc9374
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-02-13 19:07:39 +0100

    commit

--- ej9.py
+++ ej9.py
@@ -9,5 +9,28 @@ class media_notas:
     
     def media_ponderada(self,media_aritmetica): #con esta funcion,utilizando round, redondeo la media con decimales
         return round(media_aritmetica)
+  
+def introduce_nota(i):
+    while True:
+            nota = input("Introduce la {}ª nota:".format(i+1)) #para que empiece desde 1
+            try:
+                nota = float(nota)
+                break
+            except:
+                print ("Sustituya la coma por punto por favor")
+                pass
+    return nota
     
     
+if __name__ == "__main__":
+    
+   notas=[]
+   for i in range(3):  #para que solo me pida 3 notas
+       n = introduce_nota(i)
+       notas.append(n) 
+       
+    
+total = media_notas(n[0], n[1], n[2])
+    
+print("La media aritmetica es{}:".format(total.media_aritmetica()))
+print("La media ponderada es{}".format(total.media_ponderada(total.media_aritmetica())))
