d87d12fb537f5dd7a9ce4619ba9e5739d7830793
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-02-15 23:12:31 +0100

    ya funciona

--- ej8.py
+++ ej8.py
@@ -1,3 +1,6 @@
+from tkinter import N
+
+
 class prima_anual:
     def __init__(self, antiguedad,distancia,accidentes): #constructor
         self.antiguedad = antiguedad
@@ -11,5 +14,33 @@ class prima_anual:
             return 200 + 20*(self.antiguedad -4)
     
     def prima_distancia(self):   #calcula impuesto por distancia
-        return(self.distancia*0.01, 400)
+        return (self.distancia*0.01, 400)
     
+    def prima_ponderada(self, prima_antiguedad, prima_distancia): #calculo la media ponderada dependiendo del numero de accidentes
+        prima_total = prima_antiguedad + prima_distancia
+        if self.accidentes <= 3:
+            prima_total = prima_total / (self.accidentes + 1)
+        else:
+            prima_total = 0
+        return prima_total
+    
+def entrada(valor):
+    while True:
+        n = input("{}:".format(valor))
+        try:
+            n = int(n)
+            break
+        except:
+            print("Introduzca un numero por favor")
+            pass
+    return n 
+
+if __name__ == "__main__":
+    
+    antiguedad = entrada("antiguedad")
+    distancia = entrada("distancia")
+    accidentes = entrada("accidentes")
+    
+final= prima_anual(antiguedad=antiguedad, distancia=distancia, accidentes=accidentes)
+
+print("La prima ponderada es: {}".format(final.prima_ponderada(final.prima_antiguedad(),final.prima_distancia())))
