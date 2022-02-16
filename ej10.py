177511845d57068984b1d4162d34d7e9f505c201
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-02-16 17:06:25 +0100

    funciones constructor y calculo area

--- ej10.py
+++ ej10.py
@@ -1,1 +1,8 @@
 class area_triangulo:
+    def __init__(self,altura,lado): #defino el constructor
+        self.altura = altura
+        self.lado = lado
+    
+    def area(self,altura,lado):  #para calcular el area una vez tenga el valor del lado y altura
+        return (self.altura*self.lado)/2
+    
