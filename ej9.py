984c6b7b1c0e44c8e26e075d424b021293c1c5d2
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-02-18 13:07:50 +0100

    ej9 con recursividad

--- ej9.py
+++ ej9.py
@@ -1,3 +1,8 @@
+nota1 = float(input("Introduce la 1ª nota:"))
+nota2 = float(input("Introduce la 2ª nota:"))
+nota3 = float(input("Introduce la 3ª nota:"))
+
+
 class media_notas:
     def __init__(self, nota1, nota2, nota3): #defino el constructor
         self.nota1 = nota1
@@ -7,30 +12,11 @@ class media_notas:
     def media_aritmetica(self): #defino la media con decimales
         return (self.nota1 + self.nota2 + self.nota3)/3  
     
-    def media_ponderada(self,media_aritmetica): #con esta funcion,utilizando round, redondeo la media con decimales
-        return round(media_aritmetica)
-  
-def introduce_nota(i):
-    while True:
-            nota = input("Introduce la {}ª nota:".format(i+1)) #para que empiece desde 1
-            try:
-                nota = float(nota)
-                break
-            except:
-                print ("Sustituya la coma por punto por favor")
-                pass
-    return nota
-    
-    
-if __name__ == "__main__":
-    
-   notas=[]
-   for i in range(3):  #para que solo me pida 3 notas
-       n = introduce_nota(i)
-       notas.append(n) 
-       
-    
-total = media_notas(notas[0], notas[1], notas[2])
-    
-print("La media aritmetica es: {}".format(total.media_aritmetica()))
-print("La media ponderada es: {}".format(total.media_ponderada(total.media_aritmetica())))
+    def media_ponderada(self): #con esta funcion,utilizando round, redondeo la media con decimales
+        return round((self.nota1 + self.nota2 + self.nota3)/3,0)
+
+
+
+resultado = media_notas(nota1, nota2, nota3)  
+print("La media aritmetica es:", resultado.media_aritmetica())
+print("La media ponderada es:",resultado.media_ponderada())
